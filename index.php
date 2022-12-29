c99fc47094c9ddce221521fdd3e7837f28407bc7
Arsa <motivasih151@gmail.com>
2022-12-29 13:24:55 +0800

    menbambahkan sesuatu

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <<h1>Hello World!<h1> 
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi minus non sequi quisquam vitae, qui, doloremque delectus temporibus, pariatur quis officia? Incidunt enim voluptates quos. Officiis culpa ex odit! Aliquid.</p>
+    <ul>
+        <li>satu</<li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
